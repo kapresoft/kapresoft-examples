efab416a9576894253fe3ed6fc2e1f9110996a80
Tony Lagnada <deloyz@gmail.com>
2024-01-02 17:29:47 -0800

    added common-app

---
+++ common-app/src/main/java/com/kapresoft/articles/commonapp/CommonAppApplication.java
@@ -0,0 +1,13 @@
+package com.kapresoft.articles.commonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonAppApplication.class, args);
+	}
+
+}
---
+++ common-app/src/test/java/com/kapresoft/articles/commonapp/CommonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapresoft.articles.commonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
