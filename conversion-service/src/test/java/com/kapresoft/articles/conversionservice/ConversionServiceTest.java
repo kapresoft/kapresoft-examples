f8a748c0a57dea4b42f6e5a9a204df319653b27f
Tony Lagnada <deloyz@gmail.com>
2024-01-02 16:50:52 -0800

    added Set for demo.

--- conversion-service/src/test/java/com/kapresoft/articles/conversionservice/ConversionServiceTest.java
+++ conversion-service/src/test/java/com/kapresoft/articles/conversionservice/ConversionServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.TypeDescriptor;
 
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,11 +19,23 @@ class ConversionServiceTest {
     ConversionService conversionService;
 
     @Test
-    void shouldConvert_Strings_to_Numbers() {
+    void shouldConvert_ListStrings_to_Numbers() {
         List<String> stringList = List.of("1", "2", "3", "4");
         TypeDescriptor sourceType = TypeDescriptor.forObject(stringList);
         TypeDescriptor targetType = TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(Integer.class));
         List<Integer> convertedNumbers = (List<Integer>) conversionService.convert(stringList, sourceType, targetType);
+
+        assertThat(convertedNumbers).isNotNull()
+                .containsExactly(1, 2, 3, 4);
+    }
+
+    @Test
+    void shouldConvert_SetOfStrings_to_Numbers() {
+        List<String> stringList = List.of("1", "2", "3", "4");
+        TypeDescriptor sourceType = TypeDescriptor.forObject(stringList);
+        TypeDescriptor targetType = TypeDescriptor.collection(Set.class, TypeDescriptor.valueOf(Integer.class));
+        // A LinkedHashSet
+        Set<Integer> convertedNumbers = (Set<Integer>) conversionService.convert(stringList, sourceType, targetType);
 
         assertThat(convertedNumbers).isNotNull()
                 .containsExactly(1, 2, 3, 4);
