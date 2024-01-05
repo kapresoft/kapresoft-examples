26cf77dc0ee14c22d077b8ea8340264ae810e90a
Tony Lagnada <deloyz@gmail.com>
2024-01-05 11:38:01 -0800

    pom refactors. new spring-boot-api

---
+++ spring-boot-api/src/test/java/com/kapresoft/articles/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapresoft.articles.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
