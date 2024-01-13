172135c0d7d220fdeaac80ad22be04b5d8ff4cba
Tony Lagnada <deloyz@gmail.com>
2024-01-13 15:30:28 -0800

    use BusinessException

--- java-examples/src/main/java/com/kapresoft/articles/nodi/service/UserServiceImpl.java
+++ java-examples/src/main/java/com/kapresoft/articles/nodi/service/UserServiceImpl.java
@@ -1,8 +1,8 @@
 package com.kapresoft.articles.nodi.service;
 
 import com.kapresoft.articles.nodi.ApplicationContextSupport;
+import com.kapresoft.articles.nodi.exception.BusinessException;
 import com.kapresoft.articles.nodi.exception.CreateUserFailedException;
-import com.kapresoft.articles.nodi.exception.UserExistsException;
 import com.kapresoft.articles.nodi.exception.UserNotFoundException;
 import com.kapresoft.articles.nodi.model.User;
 
@@ -11,7 +11,7 @@ import static java.util.Optional.ofNullable;
 public class UserServiceImpl implements UserService, ApplicationContextSupport {
 
     @Override
-    public User registerUser(User user) throws UserExistsException {
+    public User registerUser(User user) throws BusinessException {
         userDao().createUser(user);
         String email = user.getEmail();
         var createdUser = userDao().findUserByEmail(email)
